0e3564800d21127be6eacdd2abf229dac33c1157
Alex Waweru <njorogealexw@gmail.com>
2018-02-19 12:24:33 +0000

    Implemented the factory method pattern preventing direct access to classes

---
+++ FactoryMethod2.py
@@ -0,0 +1,43 @@
+import random
+
+
+class Shape(object):
+    types = []
+
+
+def factory(type):
+
+    class Circle(Shape):
+
+        def draw(self):
+            print("Circle.draw")
+
+        def erase(self):
+            print("Circle.erase")
+
+    class Square(Shape):
+
+        def draw(self):
+            print("Square.draw")
+
+        def erase(self):
+            print("Square.erase")
+
+    if type == "Circle":
+        return Circle()
+    if type == "Square":
+        return Square()
+    assert 0, "Bad shape creation: " + type
+
+
+def shape_name_gen(n):
+    for i in range(n):
+        yield factory(random.choice(["Circle", "Square"]))
+
+# Circle() # Not defined
+
+
+for shape in shape_name_gen(7):
+    shape.draw()
+    shape.erase()
+
